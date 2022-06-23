7d493fcf20eda7568c8fff795c77a89359d67483
Dylan Gee <dylangee@gmail.com>
2022-06-23 17:23:48 +0100

    City picker function added

--- holidayPlanner.js
+++ holidayPlanner.js
@@ -7,11 +7,35 @@ const randomNum = arr => {
     }
 ;}
 
+//Using the random number generator the function picks one of these countries and returns the value
 const countryPicker = () => {
     const countries = ['Italy', 'France', 'Spain'];
-    let destination = randomNum(countries);
-    return destination;
+    let country = randomNum(countries);
+    return country;
+}; 
+
+//Value of countryPicker() stored in variable country for later use
+var country = countryPicker()
+console.log(country);
+
+//Results of countryPicker determine which array of cities should be access. Random number generator then picks a city and returns value.
+const cityPicker = () => {
+    const italy = ['Rome', 'Venice', 'Milan'];
+    const france = ['Paris', 'Nice', 'Marseille'];
+    const spain = ['Barcelona', 'Madrid', 'Sevilla'];
+    if (country === 'Italy') {
+        let city = randomNum(italy);
+        return city;
+    } else if (country === 'France') {
+        let city = randomNum(france);
+        return city;
+    }  else {
+        let city = randomNum(spain);
+        return city;
+    }   
+
 };
 
-
-console.log(countryPicker());
+//Value of cityPicker stored in variable for later use 
+var city = cityPicker();
+console.log(city);
